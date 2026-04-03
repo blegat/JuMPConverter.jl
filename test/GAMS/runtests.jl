27feaa08cd30a1b10536416b84bdb59d0ad9a6d1
Benoît Legat <benoit.legat@gmail.com>
2026-04-03 10:32:54 +0200

    Add submodules in ci (#12)

--- test/GAMS/runtests.jl
+++ test/GAMS/runtests.jl
@@ -1,3 +1,10 @@
 include(joinpath(dirname(@__DIR__), "utils.jl"))
 
+# Ensure the CACE submodule is initialized (not an empty directory)
+let cace_dir = joinpath(@__DIR__, "input", "CACE-D-21-01061", "GAMS_files")
+    if !isdir(cace_dir)
+        @warn "CACE submodule not initialized. Run `git submodule update --init`."
+    end
+end
+
 test_output(JuMPConverter.GAMS.read_model, @__DIR__)
